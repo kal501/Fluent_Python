9349c721135e0c1301cdbb26728576d452a1a81a
어그래왔니 <69739898+kal501@users.noreply.github.com>
2022-09-15 22:51:55 +0900

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,6 @@
+a = dict(one=1, two=2, three=3)
+b = {"one": 1, "two": 2, "three": 3}
+c = dict(zip(["one", "two", "three"]))
+d = dict([("two", 2), ("one", 1), ("three", 3)])
+e = dict({"three": 3, "one": 1, "two": 2})
+print(a == b == c == d == e)
