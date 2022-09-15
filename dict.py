12a812ec0a07eb7fc56136221b91429e050f35c7
어그래왔니 <69739898+kal501@users.noreply.github.com>
2022-09-15 22:54:54 +0900

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,6 +1,30 @@
-a = dict(one=1, two=2, three=3)
-b = {"one": 1, "two": 2, "three": 3}
-c = dict(zip(["one", "two", "three"]))
-d = dict([("two", 2), ("one", 1), ("three", 3)])
-e = dict({"three": 3, "one": 1, "two": 2})
-print(a == b == c == d == e)
+# BEGIN DIALCODES
+# dial codes of the top 10 most populous countries
+DIAL_CODES = [
+    (86, "China"),
+    (91, "India"),
+    (1, "United States"),
+    (62, "Indonesia"),
+    (55, "Brazil"),
+    (92, "Pakistan"),
+    (880, "Bangladesh"),
+    (234, "Nigeria"),
+    (7, "Russia"),
+    (81, "Japan"),
+]
+
+d1 = dict(DIAL_CODES)  # <1>
+print("d1:", d1.keys())
+d2 = dict(sorted(DIAL_CODES))  # <2>
+print("d2:", d2.keys())
+d3 = dict(sorted(DIAL_CODES, key=lambda x: x[1]))  # <3>
+print("d3:", d3.keys())
+assert d1 == d2 and d2 == d3  # <4>
+# END DIALCODES
+"""
+# BEGIN DIALCODES_OUTPUT
+d1: dict_keys([880, 1, 86, 55, 7, 234, 91, 92, 62, 81])
+d2: dict_keys([880, 1, 91, 86, 81, 55, 234, 7, 92, 62])
+d3: dict_keys([880, 81, 1, 86, 55, 7, 234, 91, 92, 62])
+# END DIALCODES_OUTPUT
+"""
